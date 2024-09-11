bdb5379f5fabdd56cad183f1e454975496755a91
ajrahar <miftahulfazir@gmail.com>
2024-09-11 17:59:11 +0700

    commit user site

---
+++ lib/user_screens/home_screen.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Map<String, String>> _links = []; // Ini hanya untuk menampilkan tampilan
+  List<Map<String, String>> _filteredLinks =
+      []; // Ini hanya untuk menampilkan tampilan
+
+  @override
+  void initState() {
+    super.initState();
+    // Simulasi data
+    _links = [
+      {'title': 'Google', 'link': 'https://www.google.com'},
+      {'title': 'Flutter', 'link': 'https://flutter.dev'},
+    ];
+    _filteredLinks = _links; // Memuat data yang sudah disederhanakan
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF0F172A),
+      appBar: AppBar(
+        title: const Text(
+          'Link Manager',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color(0xFF1F2937),
+      ),
+      body: Stack(
+        children: [
+          Center(
+            child: Container(
+              width: 300,
+              height: 150,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/kodegiri.png'),
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Expanded(
+                  child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 16.0,
+                      mainAxisSpacing: 16.0,
+                      childAspectRatio: 1.0,
+                    ),
+                    itemCount: _filteredLinks.length,
+                    itemBuilder: (context, index) {
+                      return _buildCard(_filteredLinks[index]);
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCard(Map<String, String> linkData) {
+    return GestureDetector(
+      onTap: () => _launchLink(linkData['link']!),
+      child: Card(
+        color: Colors.white,
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Flexible(
+                child: Text(
+                  linkData['title']!,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Flexible(
+                child: Text(
+                  linkData['link']!,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    color: Colors.black54,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _launchLink(String url) async {
+    Uri uri;
+
+    if (!url.startsWith('http://') && !url.startsWith('https://')) {
+      uri = Uri.parse('https://$url');
+    } else {
+      uri = Uri.parse(url);
+    }
+
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $uri';
+    }
+  }
+}
